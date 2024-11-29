cd17f4b398c88457d3dd67a568eae6240f268e79
CWorld <cworld0@qq.com>
2024-11-29 21:37:08 +0800

    refactor(components): :recycle: redefine user-components structure

---
+++ src/components/user/index.ts
@@ -0,0 +1,14 @@
+// Caontainer
+export { default as Card } from './Card.astro'
+export { default as Collapse } from './Collapse.astro'
+
+// List
+export { default as CardList } from './CardList.astro'
+export { default as Timeline } from './Timeline.astro'
+export { default as Steps } from './Steps.astro'
+
+// Simple text rerender
+export { default as Button } from './Button.astro'
+export { default as Spoiler } from './Spoiler.astro'
+export { default as FormattedDate } from './FormattedDate.astro'
+export { default as Label } from './Label.astro'
