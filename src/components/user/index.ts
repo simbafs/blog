453d2c1aa7aabbd2c3405920c1b730f55e904fe9
CWorld <cworld0@qq.com>
2024-11-29 23:00:17 +0800

    feat(components): :sparkles: add component tabs

--- src/components/user/index.ts
+++ src/components/user/index.ts
@@ -2,6 +2,8 @@
 export { default as Card } from './Card.astro'
 export { default as Collapse } from './Collapse.astro'
 export { default as Aside } from './Aside.astro'
+export { default as Tabs } from './Tabs.astro'
+export { default as TabItem } from './TabItem.astro'
 
 // List
 export { default as CardList } from './CardList.astro'
