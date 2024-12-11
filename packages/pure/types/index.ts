4f0d76b057f1919b1f6586e6ca25685b7291cf64
CWorld <cworld0@qq.com>
2024-12-11 14:42:55 +0800

    fix(package): :bug: remove 'virtual:types' to use relative path import; fix import path

--- packages/pure/types/index.ts
+++ packages/pure/types/index.ts
@@ -1,4 +1,27 @@
 export type { IntegrationUserConfig } from './integrations-config'
 export type { ThemeUserConfig } from './theme-config'
 export type { UserInputConfig as Config } from './user-config'
+
+export interface SiteMeta {
+  title: string
+  description?: string
+  ogImage?: string | undefined
+  articleDate?: string | undefined
+}
+
+export type CardListData = {
+  title: string
+  list: CardList
+}
+
+export type CardList = {
+  title: string
+  link?: string
+  children?: CardList
+}[]
+
+export type TimelineEvent = {
+  date: string
+  content: string
+}
 export * from 'virtual:types'
