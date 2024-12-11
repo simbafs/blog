6af2e14fceb0fc5978838015354af69242cab3c2
CWorld <cworld0@qq.com>
2024-12-11 20:37:05 +0800

    feat(package): :sparkles: support bin script like new post, info

---
+++ packages/pure/scripts/index.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+'use strict'
+
+import { readFileSync } from 'fs'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+
+import minimist from './libs/minimist.cjs'
+import newFunc from './new.mjs'
+
+const args = minimist(process.argv.slice(2))
+
+switch (args._[0]) {
+  case 'new':
+    const newArgs = process.argv.slice(3)
+    newFunc(newArgs)
+    break
+  case 'info':
+    const __dirname = dirname(fileURLToPath(import.meta.url))
+    const packageJson = JSON.parse(readFileSync(join(__dirname, '../package.json'), 'utf8'))
+    console.log()
+    console.log('\x1b[46m%s\x1b[0m', ' Astro Theme Pure ')
+    console.log('\nInformation:')
+    console.log(`- Package version: ${packageJson.version}`)
+    console.log(`- Node.js version: ${process.version}`)
+    console.log(`- Platform: ${process.platform}`)
+    break
+  case 'help':
+    console.log('Usage:')
+    console.log('  new [args] - Create new post on blog collection')
+    console.log('  info - Show this info message')
+    console.log('  help - Show this help message')
+    break
+  default:
+    console.log('Unknown command. Use "help" to see the available commands.')
+    break
+}
