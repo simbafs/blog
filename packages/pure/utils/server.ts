98bdf4daae66dac600ff5189ec70a236486f9133
CWorld <cworld0@qq.com>
2024-12-12 23:28:25 +0800

    fix(utils): :bug: fix get-blog-collection default value to 'blog'

--- packages/pure/utils/server.ts
+++ packages/pure/utils/server.ts
@@ -5,7 +5,7 @@ type Collections = CollectionEntry<CollectionKey>[]
 export const prod = import.meta.env.PROD
 
 /** Note: this function filters out draft posts based on the environment */
-export async function getBlogCollection(contentType: CollectionKey) {
+export async function getBlogCollection(contentType: CollectionKey = 'blog') {
   return await getCollection(contentType, ({ data }: CollectionEntry<typeof contentType>) => {
     // Not in production & draft is not false
     return prod ? !data.draft : true
