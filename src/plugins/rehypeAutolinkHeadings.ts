3d73274b67ff4e5095ae424ca9cdce2085bfd76d
CWorld <cworld0@qq.com>
2024-12-12 22:40:59 +0800

    perf(plugins): :zap: improve code

--- src/plugins/rehypeAutolinkHeadings.ts
+++ src/plugins/rehypeAutolinkHeadings.ts
@@ -1,8 +1,7 @@
 import type { Element, ElementContent, Root } from 'hast'
 import { visit } from 'unist-util-visit'
 
-const headingRank = (tag: string) =>
-  tag.startsWith('h') && tag.length === 2 && +tag[1] >= 1 && +tag[1] <= 6
+const headingRank = (tag: string) => /^h[1-6]$/.test(tag)
 
 export default function rehypeAutolinkHeadings({
   properties = { ariaHidden: 'true', tabIndex: -1 },
@@ -16,8 +15,19 @@ export default function rehypeAutolinkHeadings({
           type: 'element',
           tagName: 'a',
           properties: { ...properties, href: `#${node.properties.id}` },
-          children: Array.isArray(content) ? content : [content]
+          // children: Array.isArray(content) ? content : [content]
+          children: Array.isArray(content)
+            ? content
+            : typeof content === 'object' && content.type
+              ? [content]
+              : []
         }
+
+        if (!['wrap', 'prepend', 'append'].includes(behavior)) {
+          throw new Error(`Invalid behavior: ${behavior}`)
+        }
+
+        node.children = node.children || []
         if (behavior === 'wrap') {
           node.children = [link]
         } else if (behavior === 'prepend') {
