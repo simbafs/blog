540fea47fa988a5ad14d8b082dc80634bc932c3e
CWorld <cworld0@qq.com>
2024-12-28 18:41:32 +0800

    chore: :wrench: update ts & eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,13 @@
 // @ts-check
 
-import pluginJs from '@eslint/js'
+import eslint from '@eslint/js'
 import eslintPluginAstro from 'eslint-plugin-astro'
-import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default [
-  // For JavaScript and TypeScript files in general
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  pluginJs.configs.recommended,
+  eslint.configs.recommended,
   ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
-
   // Ignore files
   {
     ignores: ['public/scripts/*', 'scripts/*', '.astro/', 'src/env.d.ts']
